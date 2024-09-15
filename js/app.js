7fa03d9111a4101bc07adeb7aef30e7cda5b1f5b
Jhonathan Gois <jhogois@live.com>
2024-09-15 12:32:20 -0300

    Desafio do carrinho de compras

---
+++ js/app.js
@@ -0,0 +1,39 @@
+let totalGeral = 0
+document.getElementById('lista-produtos').innerHTML = ''
+document.getElementById('valor-total').innerHTML = 'R$0'
+
+function adicionar() {
+    //recuperar valores nome do produto, quantidade e valor
+    let produto = document.getElementById('produto').value
+    let nomeProduto = produto.split('-')[0]
+    let valorUnitario = produto.split('R$')[1]
+    let quantidade = document.getElementById('quantidade').value
+    
+    //validação da quantidade
+    if (isNaN(quantidade) || quantidade <= 0) {
+        alert('Selecione uma quantidade válida')
+        return
+    }
+    //calcular o preço, o subtotal
+
+    let preco = quantidade * valorUnitario;
+    let carrinho = document.getElementById('lista-produtos')
+    
+    //adicionar no carrinho
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto}<span class="texto-azul">R$${preco}</span>
+        </section>`
+    
+    //atualizar o valor total
+    totalGeral = totalGeral + preco
+    let campoTotal = document.getElementById('valor-total')
+    campoTotal.textContent = `R$${totalGeral}`
+    document.getElementById('quantidade').value = 1
+
+}
+
+function limpar () {
+    totalGeral = 0
+    document.getElementById('lista-produtos').innerHTML = ''
+    document.getElementById('valor-total').innerHTML = 'R$0'
+}
